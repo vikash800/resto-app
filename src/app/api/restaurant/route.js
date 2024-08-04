d323e3231a5192645ce922c79b24bff407620a6b
vikash yadav <vy851999@gmail.com>
2024-08-04 16:50:37 +0530

    fill

---
+++ src/app/api/restaurant/route.js
@@ -0,0 +1,16 @@
+import { connectionStr } from "@/aap/lib/db";
+import { restaurantSchema } from "@/app/lib/restaurantsModel";
+import mongoose from "mongoose";
+import { NextResponse } from "next/server";
+
+export async function GET(){
+    await mongoose.connect(connectionStr,{useNewUrlParser:true});
+    const data= await restaurantSchema.find()
+    console.log(data);
+      
+    return NextResponse.json({result:data})
+}
+
+export async function POST(){
+    return NextResponse.json({result:true})
+}
